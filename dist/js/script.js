cc891bd5857fbaee611b7d99345353c615523daf
Artem <lyubavin.web@gmail.com>
2021-10-21 18:29:41 +0300

    Add JS for page moving

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,31 +6,57 @@ window.addEventListener('DOMContentLoaded', () => {
         menuCal = document.querySelector('.menu__item-cal'),
         menuMass = document.querySelector('.menu__item-mass'),
         pageCal = document.querySelector('.page-cal'),
+        pageMass = document.querySelector('.page-mass'),
         btnMenuCal = document.querySelector('[data-btn="menu-cal"]'),
         btnMenuMass = document.querySelector('[data-btn="menu-mass"]'),
-        btnMainMenu = document.querySelector('[data-btn="page-main"]');
+        btnMain = document.querySelectorAll('[data-btn="page-main"]'),
+        btnPageCal = document.querySelector('[data-btn="page-mass"]'),
+        btnPageMass = document.querySelector('[data-btn="page-cal"]');
 
     function toogleMainMenu() {
         menu.classList.toggle('menu_active');
-        setTimeout(function () {
-            menuCal.classList.remove('menu__item-cal_active');
-        }, 800);
     }
 
-    btnMenuCal.addEventListener('click', () => {
+    function moveToPageFromMenu(btn, secondBtn, secondMenu, page, pageActiveClass) {
+        btn.addEventListener('click', () => {
+            secondBtn.style.display = 'none';
+            secondMenu.style.flexBasis = '0';
+            page.classList.add(pageActiveClass);
+            setTimeout(toogleMainMenu, 1000);
+            setTimeout(function() {
+                secondBtn.style.display = 'block';
+                secondMenu.style.flexBasis = '100%';
+            }, 2000);
+        });
+    }
 
-        window.location.hash = 'page';
-        menuCal.classList.add('menu__item-cal_active');
-        setTimeout(toogleMainMenu, 1000);
+    moveToPageFromMenu(btnMenuCal, btnMenuMass, menuMass, pageCal, 'page-cal_active');
+    moveToPageFromMenu(btnMenuMass, btnMenuCal, menuCal, pageMass, 'page-mass_active');
+
+
+    btnMain.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+    
+            toogleMainMenu();
+            setTimeout(function() {
+                pageCal.classList.remove('page-cal_active');
+                pageMass.classList.remove('page-mass_active');
+            }, 1000);
+        });
     });
 
-    btnMainMenu.addEventListener('click', (e) => {
-        e.preventDefault();
+    function moveToOtherPage(btn, currentPage, otherPage, currentActiveClass, otherActiveClass) {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            currentPage.classList.remove(currentActiveClass);
+            otherPage.classList.add(otherActiveClass);
+        });
+    }
 
-        menuCal.classList.remove('menu__item-cal_active');
-        toogleMainMenu();
-        window.location.hash = 'menu';
-    });
+    moveToOtherPage(btnPageCal, pageCal, pageMass, 'page-cal_active', 'page-mass_active');
+    moveToOtherPage(btnPageMass, pageMass, pageCal, 'page-mass_active', 'page-cal_active');
+    
 
     //========================================================= cal calc
     const resultCal = document.querySelector('.cal-calc__result span');
@@ -46,7 +72,7 @@ window.addEventListener('DOMContentLoaded', () => {
         if (gender === 'female') {
             resultCal.innerText = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
         } else {
-            resultCal.innerText = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5,7 * age)) * ratio);
+            resultCal.innerText = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5, 7 * age)) * ratio);
         }
     }
 
@@ -65,7 +91,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     break;
                 case "age":
                     age = +input.value;
-                    break; 
+                    break;
             }
 
             calcTotalCal();
